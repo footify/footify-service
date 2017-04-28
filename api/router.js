6c7f016b3650a8d9fbe8207e7ada0dd5d7913186
Guillaume Galas <guillaume.galas@protonmail.com>
2017-04-28 17:04:42 +0200

    Feat/qrcode (#1)
    * QRCodeController now generate QRCode url
    
    * qrcode working

--- api/router.js
+++ api/router.js
@@ -3,10 +3,7 @@ const async = require('async');
 
 function setup(app, routes) {
   _.each(routes, (route) => {
-    app[route.type](route.url, (req, res, done) => {
-      const handlers = _.map(route.handlers, (handler) => (next) => handler(req, res, next));
-      async.waterfall(handlers, done);
-    });
+    app[route.type](route.url, route.handlers);
   });
 }
 
